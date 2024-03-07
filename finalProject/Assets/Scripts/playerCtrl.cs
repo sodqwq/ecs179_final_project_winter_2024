144ecb52c718fab68cf5444df1d5ab087fc3bd09
sodqwq <57452001+sodqwq@users.noreply.github.com>
2024-03-06 22:05:40 -0800

    update

--- finalProject/Assets/Scripts/playerCtrl.cs
+++ finalProject/Assets/Scripts/playerCtrl.cs
@@ -1,14 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class playerCtrl : MonoBehaviour
 {
     Transform m_groundCheck;
-    public float m_groundCheckRadius = 0.5f;
+    public float m_groundRadius = 0.5f;
 
     Animator m_anim;
     Rigidbody2D m_body;
+    bool isFacingRight = true;
+
+    public LayerMask m_checkLayer;
+    public float Speed = 100f;
+    private void FixedUpdate()
+    {
+        bool isGround = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_groundCheck.position, m_groundRadius, m_checkLayer);
+        
+        //目前check ground 有些问题，回头修复一下，现在先保持为true
+        foreach(Collider2D c in colliders)
+        {
+            if(c.gameObject != gameObject)
+            {
+                isGround = true;
+            }
+        }
+
+        if(m_body.velocity.y == 0)
+        {
+            m_anim.SetFloat("vSpeed",0);
+        }
+        else
+        {
+            m_anim.SetFloat("vSpeed", m_body.velocity.y/Mathf.Abs(m_body.velocity.y));
+        }
+
+        float playerMoving = Input.GetAxis("Horizontal");
+        Move(playerMoving);
+    }
+    private void Move(float playerMoving)
+    {
+        Vector2 move = new Vector2(playerMoving* Speed *Time.deltaTime, 0);
+        if(playerMoving > 0)
+        {
+            if(!isFacingRight)
+            {
+                Flip();
+            }
+            m_body.velocity = move;
+            if(!m_anim.GetBool("run"))
+            {
+                m_anim.SetBool("run", true);
+            }
+        }
+        else if(playerMoving < 0)
+        {
+            if(isFacingRight)
+            {
+                Flip();
+            }
+            m_body.velocity = move;
+            if(!m_anim.GetBool("run"))
+            {
+                m_anim.SetBool("run", true);
+            }
+        }
+        else{
+            
+            if(m_anim.GetBool("run"))
+            {
+                m_anim.SetBool("run", false);
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+    }
     // Start is called before the first frame update
     void Start()
     {
