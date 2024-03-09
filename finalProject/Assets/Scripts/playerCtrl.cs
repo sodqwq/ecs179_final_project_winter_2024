0aeb4d40dfcdd7abb438b5385e2de02aa0fd61b7
Yirui99 <yirsong@ucdavis.edu@github.com>
2024-03-08 22:20:47 -0800

    Jump

--- finalProject/Assets/Scripts/playerCtrl.cs
+++ finalProject/Assets/Scripts/playerCtrl.cs
@@ -16,66 +16,76 @@ public class playerCtrl : MonoBehaviour
 
     public LayerMask m_checkLayer;
     public float Speed = 100f;
+    public float mJumpForce = 100f;
+    bool mJump;
     private void FixedUpdate()
     {
         bool isGround = false;
         Collider2D[] colliders = Physics2D.OverlapCircleAll(m_groundCheck.position, m_groundRadius, m_checkLayer);
-        
+
         //目前check ground 有些问题，回头修复一下，现在先保持为true
-        foreach(Collider2D c in colliders)
+        foreach (Collider2D c in colliders)
         {
-            if(c.gameObject != gameObject)
+            if (c.gameObject != gameObject)
             {
                 isGround = true;
             }
         }
 
-        if(m_body.velocity.y == 0)
+        if (m_body.velocity.y == 0)
         {
-            m_anim.SetFloat("vSpeed",0);
+            m_anim.SetFloat("vSpeed", 0);
         }
         else
         {
-            m_anim.SetFloat("vSpeed", m_body.velocity.y/Mathf.Abs(m_body.velocity.y));
+            m_anim.SetFloat("vSpeed", m_body.velocity.y / Mathf.Abs(m_body.velocity.y));
         }
 
         float playerMoving = Input.GetAxis("Horizontal");
-        Move(playerMoving);
+        mJump = Input.GetButtonDown("Jump");
+        Move(playerMoving, mJump);
     }
-    private void Move(float playerMoving)
+    private void Move(float playerMoving, bool jump)
     {
-        Vector2 move = new Vector2(playerMoving* Speed *Time.deltaTime, 0);
-        if(playerMoving > 0)
+        Vector2 move = m_body.velocity;
+
+        if (playerMoving > 0)// h > 0
         {
-            if(!isFacingRight)
+            move.x = playerMoving / Mathf.Abs(playerMoving) * Speed * Time.deltaTime;
+            if (!isFacingRight)
             {
                 Flip();
             }
-            m_body.velocity = move;
-            if(!m_anim.GetBool("run"))
+            if (!m_anim.GetBool("run"))
             {
                 m_anim.SetBool("run", true);
             }
         }
-        else if(playerMoving < 0)
+        else if (playerMoving < 0)
         {
-            if(isFacingRight)
+            move.x = playerMoving / Mathf.Abs(playerMoving) * Speed * Time.deltaTime;
+            if (isFacingRight)
             {
                 Flip();
             }
-            m_body.velocity = move;
-            if(!m_anim.GetBool("run"))
+            if (!m_anim.GetBool("run"))
             {
                 m_anim.SetBool("run", true);
             }
         }
-        else{
-            
-            if(m_anim.GetBool("run"))
+        else
+        {
+            move.x = 0;
+            if (m_anim.GetBool("run"))
             {
                 m_anim.SetBool("run", false);
             }
         }
+        if (jump)
+        {
+            move.y = mJumpForce * Time.deltaTime;
+        }
+        m_body.velocity = move;
     }
 
     private void Flip()
@@ -97,6 +107,6 @@ public class playerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
