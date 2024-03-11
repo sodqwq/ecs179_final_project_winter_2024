bf597f6448d41bbf2ec07412d65e861f2b71e4f6
sodqwq <57452001+sodqwq@users.noreply.github.com>
2024-03-11 01:48:19 -0700

    restart

--- finalProject/Assets/Scripts/GameManager.cs
+++ finalProject/Assets/Scripts/GameManager.cs
@@ -6,10 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        float x = PlayerPrefs.GetFloat("playerPositionX", 0);
+        float y = PlayerPrefs.GetFloat("playerPositionY", 0);
+        float z = PlayerPrefs.GetFloat("playerPositionZ", 0);
+        player.transform.position = new Vector3(x, y, z);
     }
 
     // Update is called once per frame
